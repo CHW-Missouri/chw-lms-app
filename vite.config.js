e750d41cae183c9f05951413e1c9b2b1076211f9
Nali <nalireddy@Nalis-MacBook-Air.local>
2026-04-15 17:52:05 -0500

    force deploy using docs folder

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // IMPORTANT: must match your GitHub repo name
-  base: '/chw-lms-app/',
-
+  base: "/chw-lms-app/",
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    assetsDir: 'assets'
+    outDir: "docs"
   }
-})
+});
